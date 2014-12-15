52bd1d5a0be1a908f946164c7dc5f7376fe976c9
tophat8855 <hrfarley@gmail.com>
2014-12-14 18:54:11 -0800

    search by title works

--- app/controllers/movie_search_controller.rb
+++ app/controllers/movie_search_controller.rb
@@ -1,7 +1,9 @@
-class MovieSearchController < ActionController::Base
+class MovieSearchController < ApplicationController
   def index
   end
 
   def results
+    response = RestClient.get "http://www.omdbapi.com/?s=#{params[:title]}"
+    @results = JSON.parse(response.body)
   end
 end
